775e296323ae1636917cbba7814c4ead20a02cea
Phaniraj B.N <35284581+phani-rajbn@users.noreply.github.com>
2020-12-29 08:21:07 +0530

    Add files via upload

---
+++ DhilipTraining/SampleConApp/DelegatesExample.cs
@@ -0,0 +1,63 @@
+﻿using System;
+/*
+ * U may require a function that takes another function as an argument.
+ *  They are called Callback functions. 
+ *  Delagates are used to create callback functions. 
+ *  U can create UR own delegate and call it or U could use the .NET's Action or Func delegates. 
+ *  If UR function takes a delegate object as its Arg, UR caller should create a function that matches the signature of UR delegate and pass it as argument to ur function...
+ *  All Events that U see in GUI Development are based on Events. They are all instances of a Delegate called EventHandler.
+ *  Uses: Event handling, Async Programming, Multi Threading are some of the practical usages of Delegates in .NET. 
+ *  A Delegate instance can point to one or more functions at the same time. The delegate instance pointing to multiple functions is called MultiCast delegate. Use multicast delegates for void functions as the return value of the function cannot be captured for all the functions that are casted in delegate object. U only get the last result... 
+ */
+namespace SampleConApp
+{
+    delegate void funcType();
+
+    class DelegatesExample
+    {
+        static void TestFunc(funcType funcName)
+        {
+            Console.WriteLine("UR Implementation");
+            funcName();//The function will be called inside the TestFunc...
+            Console.WriteLine("Going Back");
+        }
+        static void Main(string[] args)
+        {
+            TestFunc(myFunc);
+             
+            //MathOperation op = addFunc;
+            //MathProgram.InvokeMathOperation(op);
+
+            //using lambda Expression to instantiate a delegate...
+            //multicastExample();
+        }
+        static void myFunc() => Console.WriteLine("SimpleExample");
+
+        private static void multicastExample()
+        {
+            funcType ex = () => Console.WriteLine("Testing FuncType once");
+            ex += () => Console.WriteLine("Testing FuncType with second function");
+            ex += () => Console.WriteLine("Testing FuncType with third function");
+            ex += () => Console.WriteLine("Testing FuncType with fouth function");
+
+            ex();//call the delegate object. Delegate instance is more like an alias to Ur function...
+        }
+
+        static double subFunc(double v1, double v2) => v1 - v2;
+        static double addFunc(double v1, double v2) => v1 + v2;
+        
+    }
+
+    delegate double MathOperation(double v1, double v2);
+
+    class MathProgram
+    {
+        public static void InvokeMathOperation(MathOperation operation)
+        {
+            double num1 = Common.GetDouble("Enter First value");
+            double num2 = Common.GetDouble("Enter Second value");
+            double result = operation(num1, num2);
+            Console.WriteLine("The result: " + result);
+        }
+    }
+}
---
+++ DhilipTraining/SampleConApp/ExceptionHandling.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+/*
+ * Exception handling is handling scenarios where UR app fails to execute due to a wrong logic, wrong inputs that might not suit the execution or the system fails to take it forward. 
+ * Exceptions are raised by the CLR using objects of a class derived from System.Exception.
+ * There are 2 types: System Generated Exceptions and Application Exceptions. Application Exceptions are user defined Exceptions that are raised due to the app requirement. 
+ * Ur code can raise an Exception using throw keyword followed by an object of the Exception class with a message on why the exception has raised. 
+ * Any code that U use raises an Exception, U should catch that exception using try...catch...finally blocks...
+ * try must have catch or finally or both.
+ * try block is the section of the code that U want to execute, but could raise an Exception. 
+ * catch block contains the Exception handling block. U can have multiple catch blocks to suit various kinds of Exceptions. 
+ * finally block is optional and will run all the time irrespective whether an Exception occurs or not. It is more like a cleanup Operation..
+ * Exceptions that are not caught are unhandled and will stop the program from executing. 
+ */
+namespace SampleConApp
+{
+    class ExceptionHandling
+    {
+        static void Main(string[] args)
+        {
+            //usingTryCatchFinally();
+            //usingMultipleCatchExample();
+            //usingDivideByZeroException();
+            //FileHandlingExceptionExample();
+            //Custom Exceptions will be covered later.
+        }
+
+        private static void FileHandlingExceptionExample()
+        {
+            try
+            {
+                string data = File.ReadAllText("Example.txt");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("This file does not exist");
+            }
+        }
+
+        private static void usingDivideByZeroException()
+        {
+            Console.WriteLine("Lets write a Division program");
+            TRYAGAIN:
+            try
+            {
+                int num1 = Common.GetNumber("Enter First Value");
+                int num2 = Common.GetNumber("Enter Second Value");
+                int res = num1 / num2;
+                Console.WriteLine(res);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Input is not a valid Integer, Please try again");
+                goto TRYAGAIN;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Integer value should be within the range of {int.MinValue} and {int.MaxValue}");
+                goto TRYAGAIN;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Denominator should not be zero while U divide a number. Please try again");
+                goto TRYAGAIN;
+            }
+        }
+
+        private static void usingMultipleCatchExample()
+        {
+            TRYAGAIN:
+            try
+            {
+                int no = Common.GetNumber("Enter the Number");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Input is not a valid Integer, Please try again");
+                goto TRYAGAIN;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Integer value should be within the range of {int.MinValue} and {int.MaxValue}");
+                goto TRYAGAIN;
+            }
+        }
+
+        private static void usingTryCatchFinally()
+        {
+            try
+            {
+                throw new Exception("Simply raised to show an Example");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Close the code");
+            }
+        }
+    }
+}
